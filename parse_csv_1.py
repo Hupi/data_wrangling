3fe384b8748193791fd2e7dc9bc2b8be79dd9c99
Fangjie <liufangjie1987@gmail.com>
2017-11-01 15:12:18 -0700

    simple scripts to extract data from csv, excel, json

---
+++ parse_csv_1.py
@@ -0,0 +1,23 @@
+# using the csv module
+import os
+import pprint
+import csv
+
+DATADIR = ""
+DATAFILE ="beatles_diskography.csv"
+
+def parse_csv(datafile):
+	data = []
+	n = 0
+	with open(datafile, 'rb') as sd:
+		r = csv.DictReader(sd)
+		for line in r:
+			data.append(line)
+	return data
+
+
+if __name__ == '__main__':
+	datafile = os.path.join(DATADIR,datafile)
+	parse_csv(datafile)
+	d = parse_csv(datafile)
+	pprint.pprint(d)
